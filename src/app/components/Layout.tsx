6d12a724f4cf82a8d9c6f4f8bc767e5d0108302c
akshay <jakshay18397@gmail.com>
2026-06-01 11:19:50 +0530

    push code

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router';
 import { Paintbrush, Phone, Mail, ChevronDown, Menu, X, Facebook, Twitter, Youtube } from 'lucide-react';
+import myLocalImage from '../assets/brand-logo.png';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [servicesOpen, setServicesOpen] = useState(false);
@@ -59,7 +60,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <Link to="/" className="flex items-center gap-2">
               {/* <Paintbrush className="w-6 h-6 md:w-7 md:h-7 text-accent" />
               <span className="font-bold text-lg md:text-xl text-primary">Pandit Painting Services</span> */}
-              <img src="/assets/brand-logo.png" alt="" />
+              <img src={myLocalImage} alt="" />
             </Link>
 
             {/* Mobile Menu Toggle */}
